4785afebd2b0579f6ef83f725841e4e2981e387a
Vrushali-Adhav <113894503+Vrushali-Adhav@users.noreply.github.com>
2022-11-27 19:08:57 +0530

    Add files via upload

---
+++ Scanner_nextint.java
@@ -0,0 +1,23 @@
+/*Program Name : Program for Scanner nextInt
+Programmer's Name : Adhav  Vrushali Abasaheb
+Programming Language:Java Programming Language
+Date                 : 11 Nov 2022
+*/
+import java.util.Scanner;
+class Scanner_nextint
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int a,b,c;
+		System.out.println("*******Substraction*************");
+		System.out.println("enter value of a");
+		a = sc.nextInt();
+		System.out.println("enter value of b");
+		b = sc.nextInt();
+		c = a-b;
+		System.out.println("substraction = "+c);
+	}
+}
+		
+		
